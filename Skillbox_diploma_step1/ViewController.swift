dc221e6c9ebca8093ed2c93801ab8ec6e698dad6
Roman Sundurov <roman.sundurov.work@gmail.com>
2021-01-07 15:19:43 +0300

    Initial Commit

---
+++ Skillbox_diploma_step1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skillbox_diploma_step1
+//
+//  Created by Roman on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
