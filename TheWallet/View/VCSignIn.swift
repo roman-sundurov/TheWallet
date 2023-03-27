c694211dd123e6f5b44c511baa2dcaea78d7e50f
Roman Sundurov <roman.sundurov.work@gmail.com>
2023-03-27 03:33:04 +0300

    Fixed keyboard avoiding in vcSignIn.

--- TheWallet/View/VCSignIn.swift
+++ TheWallet/View/VCSignIn.swift
@@ -24,11 +24,13 @@ class VCSignIn: UIViewController {
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextView: RoundedView!
     @IBOutlet var passwordTextField: UITextField!
-
     @IBOutlet var emailSignInButton: UIButton!
     @IBOutlet var emailSignUpButton: UIButton!
-
     @IBOutlet var appleSignInButton: AppleSignInShadowButton!
+
+    @IBOutlet var scrollViewBottomConstraint: NSLayoutConstraint!
+    var keyboardHeight: CGFloat = 0 // хранит высоту клавиатуры
+    var tapOutsideTextViews: UITapGestureRecognizer?
 
     @IBAction func signInButton(_ sender: Any) {
         do {
@@ -70,10 +72,40 @@ class VCSignIn: UIViewController {
             name: ASAuthorizationAppleIDProvider.credentialRevokedNotification,
             object: nil
         )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillDisappear),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillAppear),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tapOutsideTextViews = UITapGestureRecognizer(
+            target: self,
+            action: #selector(self.tapHandler(tap:))
+        )
+        if let tapOutsideTextViews = tapOutsideTextViews {
+            self.view.addGestureRecognizer(tapOutsideTextViews)
+        } else {
+            // showAlert(message: "Error addGestureRecognizer")
+            print("Error: VCSignIn addGestureRecognizer")
+        }
+
+        emailTextField.delegate = self
+        emailTextField.inputAccessoryView = createDoneButton()
+        passwordTextField.delegate = self
+        passwordTextField.inputAccessoryView = createDoneButton()
 
         UserRepository.shared.listener = Auth.auth().addStateDidChangeListener() { (auth, user) in
             if let user = user,
@@ -88,41 +120,4 @@ class VCSignIn: UIViewController {
         }
     }
 
-    // MARK: - other functions
-    @objc func appleIDStateDidRevoked(_ notification: Notification) {
-            // Make sure user signed in with Apple
-        if let providerId =  Auth.auth().currentUser?.providerData.first?.providerID,
-           providerId == "apple.com" {
-            UserRepository.shared.logOut()
-        }
-    }
-
-    func showMessagePrompt(_ message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert.addAction(okAction)
-        present(alert, animated: false, completion: nil)
-    }
-
-    private func showTextInputPrompt(
-        withMessage message: String,
-        completionBlock: @escaping ((Bool, String?) -> Void)
-    ) {
-        let prompt = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
-            completionBlock(false, nil)
-        }
-        weak var weakPrompt = prompt
-        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            guard let text = weakPrompt?.textFields?.first?.text else { return }
-            completionBlock(true, text)
-        }
-        prompt.addTextField(configurationHandler: nil)
-        prompt.addAction(cancelAction)
-        prompt.addAction(okAction)
-        present(prompt, animated: true, completion: nil)
-    }
-
 }
-
-
--- TheWallet/ViewModel/SignInVM.swift
+++ TheWallet/ViewModel/SignInVM.swift
@@ -14,6 +14,7 @@ import AuthenticationServices
 
 extension VCSignIn {
 
+    //MARK: - signIn functions
     func emailSignIn() throws {
         emailSignInButton.configuration?.showsActivityIndicator = true
 
@@ -81,7 +82,125 @@ extension VCSignIn {
 
             }
         }
+    }
 
+    @objc func appleIDStateDidRevoked(_ notification: Notification) {
+            // Make sure user signed in with Apple
+        if let providerId =  Auth.auth().currentUser?.providerData.first?.providerID,
+           providerId == "apple.com" {
+            UserRepository.shared.logOut()
+        }
+    }
+
+    func showMessagePrompt(_ message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: false, completion: nil)
+    }
+
+    private func showTextInputPrompt(
+        withMessage message: String,
+        completionBlock: @escaping ((Bool, String?) -> Void)
+    ) {
+        let prompt = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            completionBlock(false, nil)
+        }
+        weak var weakPrompt = prompt
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            guard let text = weakPrompt?.textFields?.first?.text else { return }
+            completionBlock(true, text)
+        }
+        prompt.addTextField(configurationHandler: nil)
+        prompt.addAction(cancelAction)
+        prompt.addAction(okAction)
+        present(prompt, animated: true, completion: nil)
+    }
+
+    // MARK: - keyboard avioding
+    @objc func tapHandler(tap: UITapGestureRecognizer) {
+        if tap.state == UIGestureRecognizer.State.ended {
+            print("Tap TextView ended")
+            let pointOfTap = tap.location(in: self.view)
+            if !emailTextView.frame.contains(pointOfTap) &&
+                !passwordTextView.frame.contains(pointOfTap) {
+                emailTextView.endEditing(true)
+                passwordTextView.endEditing(true)
+                print("Tap inside TextViews")
+            }
+        }
+    }
+
+    func tapOutsideTextViewEditToHide() {
+        emailTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        print("textViewDeselect")
+    }
+
+    @objc func keyboardWillAppear(_ notification: Notification) {
+        print("keyboardWillAppear")
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            keyboardHeight = keyboardRectangle.height
+        }
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 0,
+            options: UIView.AnimationOptions(),
+            animations: {
+                if self.scrollViewBottomConstraint.constant == 0 {
+                    self.scrollViewBottomConstraint.constant = self.keyboardHeight + CGFloat.init(0)
+                }
+                self.view.layoutIfNeeded()
+            },
+            completion: { _ in }
+        )
+    }
+
+    @objc func keyboardWillDisappear(_ notification: Notification) {
+        if keyboardHeight != 0 {
+            print("keyboardWillDisappear")
+            keyboardHeight = 0
+            UIView.animate(
+                withDuration: 0.5,
+                delay: 0,
+                usingSpringWithDamping: 0.8,
+                initialSpringVelocity: 0,
+                options: UIView.AnimationOptions(),
+                animations: {
+                    if self.scrollViewBottomConstraint.constant > 0 {
+                        self.scrollViewBottomConstraint.constant = CGFloat.init(0)
+                    }
+                    self.view.layoutIfNeeded()
+                },
+                completion: { _ in }
+            )
+        }
+    }
+
+}
+
+extension VCSignIn: UITextFieldDelegate {
+    func createDoneButton() -> UIView {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = UIBarStyle.default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        return doneToolbar
+    }
+
+    @objc func doneButtonAction() {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
     }
 
 }
